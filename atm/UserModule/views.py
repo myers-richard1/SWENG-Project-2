afc62d664ba4783162e6c9e22c3b0df32f7bff71
Richard Myers <myers_richard1@columbusstate.edu>
2020-11-20 18:55:42 -0500

    Added comments to views.py

--- atm/UserModule/views.py
+++ atm/UserModule/views.py
@@ -6,17 +6,23 @@ import datetime
 from datetime import timedelta, date
 import decimal
 
-
 # Create your views here.
-#get
+#GET
+#this function simply displays the login page
 def user_home(request):
     context = {
         "form":LoginForm
-
     }
     return render(request, 'user_home.html', context)
 
-#post
+#POST
+#this function accepts post data from user_home.html and 
+#verifies that the credentials match what's in the database.
+#on success, it redirects to user to user_options.
+#on failure, it redurects to login_fail.
+#POST parameters:
+#   card_number:    the user's card number
+#   pin:            the user's pin
 def verify(request):
     card_num = request.POST['card_number']
     pin = request.POST['pin']
@@ -29,20 +35,27 @@ def verify(request):
     print("Redirecting...")
     return HttpResponseRedirect("/%s/options" % card_num)
 
+#GET
+#this view is basically a copy of user_home with an additional 
+#error message notifying the user their attempted login was invalid
 def login_fail(request):
     context = {'form':LoginForm}
     return render(request, "login_fail.html", context)
 
-#post
+#POST
+#this function accepts post data from account_creation_options.html.
+#it creates both a new account, and a new card tied to that account
+#POST parameters:
+#   name:           the user's real name
+#   phone_number:   the user's phone number
+#   pin:            the user's new pin
+#   address:        the user's address
 def create_account(request):
-    #account fields: account number (auto), name, phone number, balance=0
     new_name = request.POST['name']
     new_phone_number = request.POST['phone_number']
     new_account = Account(name = new_name, phone_number = new_phone_number, balance = 10000.0)
     new_account.save()
 
-    #card fields: card_number(auto) account num (fk), pin, 
-    #   issue date, exp date, address, twoFA, phone_num, card_status
     new_pin = request.POST['pin']
     new_address = request.POST['address']
     today = datetime.date.today()
@@ -55,12 +68,15 @@ def create_account(request):
     
     return HttpResponseRedirect('%s/options/' % new_card.card_number)
 
-#get
+#GET
+#this function simply displays the account creation form
 def account_creation_options(request):
     context = {'form':AccountCreationForm}
     return render(request, "account_creation_options.html", context)
 
-#get
+#GET
+#this function verifies the card exists and then renders
+#the user_options page
 def user_options(request, card_num):
     try:
         card = Card.objects.get(pk=card_num)
@@ -69,13 +85,19 @@ def user_options(request, card_num):
     context = {'card_num' : card_num, 'name' : "test"}
     return render(request, 'user_options.html', context)
 
-#get
+#GET
+#this function simply displays the withdrawal form
 def withdraw(request, card_num):
     context = {'card_num' : card_num, 'form':WithdrawalForm}
     return render(request, 'withdraw.html', context)
 
-#post
+#POST
+#this function processes the withdrawal and updates the 
+#account balance.
+#POST parameters:
+#   amount: the amount to withdraw from the account
 def process_withdrawal(request, card_num):
+    #get card and account
     try:
         card = Card.objects.get(pk=card_num)
     except Card.DoesNotExist:
@@ -84,14 +106,14 @@ def process_withdrawal(request, card_num):
         account = Account.objects.get(card=card_num)
     except Account.DoesNotExist:
         return HttpResponse("Could not find account")
+    #get amount from POST data and update balance
     amount = float(request.POST['amount'])
     if (amount > account.balance):
         return HttpResponse("Account balance ($%.2f) is lower than requested amount ($%.2f). TODO implement error handler" % (account.balance, amount))
     else:
         account.balance -= decimal.Decimal(amount)
         account.save()
-    
-    #TODO use actual atm instead of defaulting to ATM 1
+    #create transaction record
     atm = ATMachine.objects.get(pk=1)
     transaction = CashWithdrawalTransaction(card_number=card, ATM_UID=atm, status="Approved",
         date_of_transaction=date.today(), response_code="100", transaction_type="Withdrawal", amount_transferred=amount,
@@ -99,7 +121,8 @@ def process_withdrawal(request, card_num):
     transaction.save()
     return HttpResponseRedirect('/%s/withdrawal_results' % card_num)
 
-#get
+#GET
+#this function simply displays the withdrawal results
 def withdrawal_results(request, card_num):
     try:
         account = Account.objects.get(card = card_num)
@@ -109,13 +132,21 @@ def withdrawal_results(request, card_num):
     context = {'card_num':card_num, 'balance':balance_str}
     return render(request, "withdrawal_results.html", context)
 
-#get
+#GET
+#this function simply displays the transfer form
 def transfer(request, card_num):
     context = {'card_num':card_num}
     return render(request, 'transfer.html', context)
 
-#post
+#POST
+#this function verifies the transfer information and
+#balance, and then updates the account balances.
+#redirects to transfer_results
+#POST parameters:
+#   beneficiary:    the account number of the beneficiary account
+#   amount:         the amount to transfer
 def process_transfer(request, card_num):
+    #get beneficiary account and user card references
     beneficiary = request.POST['beneficiary']
     try:
         account = Account.objects.get(pk=beneficiary)
@@ -125,16 +156,18 @@ def process_transfer(request, card_num):
         card = Card.objects.get(pk=card_num)
     except Card.DoesNotExist:
         return HttpResponse("Invalid card number.")
+    #get amount from post data update balance
     amount = float(request.POST['amount'])
     if (amount > card.account.balance):
         return HttpResponse("Account balance ($%.2f) is lower than requested amount ($%.2f). TODO implement error handler" % (card.account.balance, amount))
     else:
+        #convert amount to decimal
         amount_dec = decimal.Decimal(amount)
         account.balance += amount_dec
         account.save()
         card.account.balance -= amount_dec
         card.account.save()
-    #TODO use actual atm instead of defaulting to the first one
+    #create transaction record
     atm = ATMachine.objects.get(pk=1)
     transaction = CashTransferTransaction(card_number=card, ATM_UID=atm, status="Approved",
         date_of_transaction=date.today(), response_code="100", transaction_type="Transfer",
@@ -142,7 +175,9 @@ def process_transfer(request, card_num):
     transaction.save()
     return HttpResponseRedirect('/%s/%s/transfer_results' % (card_num, beneficiary))
 
-#get
+#GET
+#this function verifies the card and beneficiary account exists,
+#and renders the results of the transfer
 def transfer_results(request, card_num, beneficiary):
     try:
         card = Card.objects.get(pk=card_num)
@@ -156,7 +191,9 @@ def transfer_results(request, card_num, beneficiary):
     context = {'card_num':card_num, 'beneficiary':beneficiary, 'balance':balance_str}
     return render(request, "transfer_results.html", context)
 
-#get
+#GET
+#this function verifies the card exists and 
+#displays the balance.
 def check_balance(request, card_num):
     try:
         card = Card.objects.get(pk=card_num)
@@ -166,12 +203,16 @@ def check_balance(request, card_num):
     context = {'card_num': card_num, 'balance':balance_str}
     return render(request, "check_balance.html", context)
 
-#get
+#GET
+#this function verifies the card exists,
+#and iterates through all of the transactions involving that card,
+#then adds them to the page context so they can be displayed
 def view_transactions(request, card_num):
     try:
         card = Card.objects.get(pk=card_num)
     except Card.DoesNotExist:
         return HttpResponse("Invalid card number.")
+
     transfer_str = ""
     transactions_exist = False
     try:
